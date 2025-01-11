4224be08436a8203ad62783e6d8d9eca1b8faea8
matthew-weolbu <159215070+matthew-weolbu@users.noreply.github.com>
2025-01-11 20:44:09 +0900

    config : 초기 환경 셋팅

---
+++ src/main/kotlin/com/pulley/freewheelin/PulleyApplication.kt
@@ -0,0 +1,11 @@
+package com.pulley.freewheelin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PulleyApplication
+
+fun main(args: Array<String>) {
+  runApplication<PulleyApplication>(*args)
+}
---
+++ src/test/kotlin/com/pulley/freewheelin/PulleyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pulley.freewheelin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PulleyApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
